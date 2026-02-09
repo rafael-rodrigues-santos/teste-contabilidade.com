47d605da25d008ac18d948b413b3ca519b37c341
Rafael <rafael.santos@bethebeth.com.br>
2026-02-09 12:57:55 -0300

    chore: Adiciona HeroSection ao índice de componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { Button } from './Button/Button';
 export { Heading } from './Heading/Heading';
+export { HeroSection } from './HeroSection/HeroSection';
 export { Paragraph } from './Paragraph/Paragraph';
 export { PlanCard } from './PlanCard/PlanCard';
 export { PlansSection } from './PlansSection/PlansSection';
